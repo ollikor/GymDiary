4fd7b031f6dd44e7dac9373dad8331c4095d5080
olli <olli.korhonen19@gmail.com>
2024-01-26 07:46:16 +0200

    init

---
+++ GymDiary/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * GymDiary
+ * 
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View, Text } from 'react-native'
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>GymDiary</Text>
+    </View>
+  );
+}
+
+export default App;
